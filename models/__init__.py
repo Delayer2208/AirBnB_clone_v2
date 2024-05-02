e224d246c20a36e3e388038cbf40c47f35c3862b
delayer2208 <Veresan72@gmail.com>
2024-05-02 11:15:56 +0200

    Finish

--- models/__init__.py
+++ models/__init__.py
@@ -4,11 +4,17 @@ This module creates an instance of a FileStorage object.
 """
 from os import getenv
 
+def get_storage_type():
+    """
+    Returns the type of storage.
+    """
+    return getenv("HBNB_TYPE_STORAGE")
+
 def get_storage():
     """
     Returns an instance of the appropriate storage engine.
     """
-    is_type = getenv("HBNB_TYPE_STORAGE")
+    is_type = get_storage_type()
     
     if is_type == 'db':
         from models.engine.db_storage import DBStorage
